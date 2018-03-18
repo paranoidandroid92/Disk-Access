ead4697e017e14dde5cd76236dbd809e11a03b3a
Ali ATIL <alihaydar.atil@gmail.com>
2018-03-18 22:56:18 +0300

    source code

--- src/disk.asm
+++ src/disk.asm
@@ -12,6 +12,12 @@ mov [drive_index],dl
 
 end:
 	call check_bios_extension
+	mov ax,0
+	mov es,ax
+	mov di,0x7e00
+	mov ax,0
+	mov bx,1
+	call read_sector
 	hlt
 	jmp end
 
@@ -21,19 +27,42 @@ check_bios_extension:
 	mov dl,[drive_index]
 	mov bx,0x55AA
 	int 0x13
-	jc ext_present
+	jc ext_not_present
 ext_present:
 	mov si,ext_present_str
 	jmp ext_end
 ext_not_present:
 	mov si,ext_not_present_str
-	jmp ext_end
 ext_end:
 	call printStr
 	popa
 	ret
-	
 
+;es:di => read location
+;ax    => number of first sector
+;bx    => number of sectors to be read
+read_sector:
+	pusha
+	push es
+	mov [dap_num_of_secs],bx
+	mov [dap_seg_addr],es
+	mov [dap_off_addr],di
+	mov [dap_start_sec],ax
+	mov ah,0x42
+	mov dl,[drive_index]
+	mov si,disk_access_packet
+	int 0x13
+	jc read_fail
+read_succesful:
+	mov si,read_succesful_str
+	jmp read_end
+read_fail:
+	mov si,read_fail_str
+read_end:
+	call printStr
+	pop es
+	popa
+	ret
 
 ;prints string in [ds:si] to screen	
 printStr:
@@ -75,11 +104,20 @@ printStrFinish:
 	popa;
 	ret;
 
-ext_present_str db 'Extension present',0x00
-ext_not_present_str db 'Extension not present',0x00
+ext_present_str db 'Extension present', 0x0A, 0x00
+ext_not_present_str db 'Extension not present', 0x0A, 0x00
+read_succesful_str db 'Read succesful', 0x0A, 0x00
+read_fail_str db 'Read failed', 0x0A, 0x00
 video_memory_base dw 0xb800
-video_memory_index dw 0x0000
-drive_index db 0x00
+video_memory_index dw 0
+drive_index db 0
+disk_access_packet:
+	dap_size 		db 0x10
+			 		db 0
+	dap_num_of_secs dw 0x0000
+	dap_off_addr	dw 0
+	dap_seg_addr	dw 0
+	dap_start_sec	dq 0
 times 510-($-$$) db 0x00
 db 0x55
 db 0xAA
